7c7c3f7aa949ee0047b3f69ed1e9838c193d67d6
puang59 <kk61468@gmail.com>
2023-11-01 17:51:49 +0530

    beautified menu page

--- fileease.py
+++ fileease.py
@@ -1,55 +1,55 @@
 #!/usr/bin/env python3
+from public.logoascii import logo
 
-import os
-import shutil
-import click
-
+from click import echo, prompt, IntRange, style
+import logging
 from tabulate import tabulate
 
 from modules._managing import manager
 from modules._encrypt import encrypt
-from modules._decrypt import decrypt 
+from modules._decrypt import decrypt
+from modules._compress import compress
+from modules._extract import extract
 
-@click.command()
-def mainMenu():
-    logo_ascii = '''
-    /$$$$$$$$ /$$ /$$           /$$$$$$$$
-   | $$_____/|__/| $$          | $$_____/
-   | $$       /$$| $$  /$$$$$$ | $$        /$$$$$$   /$$$$$$$  /$$$$$$
-   | $$$$$   | $$| $$ /$$__  $$| $$$$$    |____  $$ /$$_____/ /$$__  $$
-   | $$__/   | $$| $$| $$$$$$$$| $$__/     /$$$$$$$|  $$$$$$ | $$$$$$$$
-   | $$      | $$| $$| $$_____/| $$       /$$__  $$ \____  $$| $$_____/
-   | $$      | $$| $$|  $$$$$$$| $$$$$$$$|  $$$$$$$ /$$$$$$$/|  $$$$$$$
-   |__/      |__/|__/ \_______/|________/ \_______/|_______/  \_______/
-    '''
+logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-    colored_logo = click.style(logo_ascii, fg='green', bold=True)
-    name = click.style('~ puang59 \n', fg='red', bold=True)
-    click.echo(colored_logo)
-    click.echo(name)
-
-    click.echo(f"      {click.style('##### MENU #####', fg='cyan', bold=True)}")
+def display_menu():
+    echo(logo)
+    echo(tabulate([['Your Ultimate CLI Companion for everything Files and Folders related!']], tablefmt="fancy_grid", numalign="center", stralign="center"))
+    echo(style("~ puang59\n", fg='red', bold=True))
+    echo(f"{style('######## MENU ########', fg='cyan', bold=True)}")
     menu = [
-        ["1", "Manage Files"],
-        ["2", "Encrypt Files"],
-        ["3", "Decrypt Files"],
-        ["4", "Compress Files"],
+        [style("1)", fg="yellow"), style("Manage Files", fg="white")],
+        [style("2)", fg="yellow"), style("Encrypt Files", fg="white")],
+        [style("3)", fg="yellow"), style("Decrypt Files", fg="white")],
+        [style("4)", fg="yellow"), style("Compress Files", fg="white")],
+        [style("5)", fg="yellow"), style("Extract Files", fg="white")]
     ]
-    click.echo(tabulate(menu, headers=["Option", "Description"], tablefmt="grid"))
 
-    option = click.prompt('\nSelect an option (1/2/3)', type=int)
+    echo(tabulate(menu, tablefmt="fancy_grid"))
 
-    if option == 1:
-        click.echo(f"\n{click.style('1) Manage Files', fg='magenta', bold=True)}")
-        manager()
-    elif option == 2:
-        click.echo(f"\n{click.style('2) Encrypt Files', fg='magenta', bold=True)}")
-        encrypt()
-    elif option == 3:
-        click.echo(f"\n{click.style('3) Decrypt Files', fg='magenta', bold=True)}")
-        decrypt()
-    else:
-        click.echo("Yet to release!")
+def main():
+    try:
+        options = {
+            1: manager,
+            2: encrypt,
+            3: decrypt,
+            4: compress,
+            5: extract
+        }
         
+        while True:
+            display_menu()
+            option = prompt('\nSelect an option (1/2/3/4/5)', type=IntRange(1, 5))
+
+            selected_function = options.get(option)
+            if selected_function:
+                selected_function()
+            else:
+                echo("Invalid option. Please select a valid option.")
+    except Exception as e:
+        logging.error(str(e))
+        echo(f"An error occurred. Please check the log file for details.")
+
 if __name__ == '__main__':
-    mainMenu()
+    main()
---
+++ modules/_compress.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import click
+import os
+import zipfile
+
+@click.command()
+def compress():
+    try:
+        # Ask the user for the directory containing the files to compress
+        directory = click.prompt('Enter the directory containing the files to compress', type=click.Path(exists=False, file_okay=False, dir_okay=True))
+        directory = os.path.expanduser(directory)  # Expand the ~ to the home directory
+
+        # Validate if the expanded directory exists
+        if not os.path.exists(directory) or not os.path.isdir(directory):
+            click.echo(f"Directory '{directory}' does not exist.")
+            return
+
+        # List files in the specified directory
+        files = os.listdir(directory)
+
+        if not files:
+            click.echo(f"No files found in the directory: {directory}")
+            return
+
+        click.echo("Files in the directory:")
+        for index, file in enumerate(files, start=1):
+            index = click.style(str(index), fg='green')
+            click.echo(f"{index}. {file}")
+
+        zip_filename = click.prompt('Enter a name for the ZIP archive (e.g., my_archive)', type=str)
+
+        # Add .zip extension if not provided
+        if not zip_filename.endswith('.zip'):
+            zip_filename += '.zip'
+
+        zip_location = click.prompt('Enter the location to store the ZIP file', type=str)
+
+        # Expand the ~ in the path to the home directory
+        zip_location = os.path.expanduser(zip_location)
+
+        # Create a ZIP archive with the specified name and location
+        zip_filepath = os.path.join(zip_location, zip_filename)
+
+        with zipfile.ZipFile(zip_filepath, 'w', zipfile.ZIP_DEFLATED) as zipf:
+            for file in files:
+                file_path = os.path.join(directory, file)
+                zipf.write(file_path, arcname=file)
+
+        zip_filename = click.style(zip_filename, fg='magenta')
+        click.echo(f"\nFiles in the directory have been compressed into {zip_filename}")
+
+    except Exception as e:
+        click.echo(f"Error: {str(e)}")
+
+if __name__ == '__main__':
+    compress()
